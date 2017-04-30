b4eefb3ffc3951840545bcbf6604fc0f27c772f8
tangjiong <330194737@qq.com>
2017-04-30 20:28:54 +0800

    put frontend pages into backend dir

--- src/geek-report/src/clj/geek_report/routes/home.clj
+++ src/geek-report/src/clj/geek_report/routes/home.clj
@@ -5,13 +5,8 @@
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
-
-(defn about-page []
-  (layout/render "about.html"))
+  (layout/render "index.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page)))
 
